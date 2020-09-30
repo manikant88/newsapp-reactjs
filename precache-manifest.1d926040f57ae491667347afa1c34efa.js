2937312e92c990dd60a01e1ff63e14267a2519ff
Manikant <manikant.mudgil@pixacore.in>
2020-10-01 02:14:45 +0530

    Updates

---
+++ precache-manifest.1d926040f57ae491667347afa1c34efa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7e72b47b7f2a39efc34f3c27af12de3",
+    "url": "/newsapp-reactjs/index.html"
+  },
+  {
+    "revision": "6874b473f779e9d42031",
+    "url": "/newsapp-reactjs/static/css/main.9e788188.chunk.css"
+  },
+  {
+    "revision": "8630d54a1a9b69a6b869",
+    "url": "/newsapp-reactjs/static/js/2.4f1ba8b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/newsapp-reactjs/static/js/2.4f1ba8b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6874b473f779e9d42031",
+    "url": "/newsapp-reactjs/static/js/main.75858865.chunk.js"
+  },
+  {
+    "revision": "0019253d27554345e8fc",
+    "url": "/newsapp-reactjs/static/js/runtime-main.97721740.js"
+  },
+  {
+    "revision": "70ebbcd163470cdcdd46617ad9e91610",
+    "url": "/newsapp-reactjs/static/media/like.70ebbcd1.svg"
+  },
+  {
+    "revision": "c12d9fd4259675a2773d51e7c0d4e26d",
+    "url": "/newsapp-reactjs/static/media/liked.c12d9fd4.svg"
+  }
+]);
